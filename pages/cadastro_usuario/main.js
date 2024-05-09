f4392487abd261e3bfe413e0b917abc14fa00ea4
Rogerio Jesuino Ferraz Rocha <139191290+RogerioJesuinoFR@users.noreply.github.com>
2024-05-09 14:52:28 -0300

    created basic JS

--- pages/cadastro_usuario/main.js
+++ pages/cadastro_usuario/main.js
@@ -1,20 +1,53 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    
+    function limparCampos() {
+        document.getElementById("nomeInput").value = "";
+        document.getElementById("emailInput").value = "";
+        document.getElementById("senhaInput").value = "";
+        document.getElementById("confSenhaInput").value = "";
+    }
+    
     function validaFormulario() {
-        if (document.getElementById("nome").value == "" || 
-            document.getElementById("email").value == "" || 
-            document.getElementById("assunto").value == "" || 
-            document.getElementById("msg").value == "") {
-                return false;
+        var nomeCompleto = document.getElementById("nomeInput").value;
+        var email = document.getElementById("emailInput").value;
+        var senha = document.getElementById("senhaInput").value;
+        var confirmaSenha = document.getElementById("confSenhaInput").value;
+
+        if (nomeCompleto.trim().indexOf(' ') === -1) {
+            return 2; // Não inseriu o sobrenome
+        }
+
+        var partesNome = nomeCompleto.split(' ');
+        var nome = partesNome[0];
+        var sobrenome = partesNome.slice(1).join(' ');
+
+        if (nome === "" || email === "" || senha === "" || confirmaSenha === "") {
+            return false; // Campos em branco
+        } else if (senha.length < 6) {
+            return 1; // Senha muito curta
+        } else if (confirmaSenha !== senha) {
+            return 0; // Senhas não coincidem
         } else {
-            return true;
+            return true; // Formulário válido
         }
     }
 
-    document.getElementById("enviar").addEventListener("click", function() {
-        if (validaFormulario()) {
+    document.querySelector("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        var validacao = validaFormulario();
+        if (validacao === true) {
             alert("Formulário válido. Enviando...");
-        } else {
-            alert("Por favor, preencha todos os campos do formulário!");
+            limparCampos();
+        } else if (validacao === false) {
+            alert("Por favor, preencha todos os campos do formulário corretamente!");
+        } else if (validacao === 0) {
+            alert("As senhas não combinam, favor insira a senha utilizada para confirmação");
+        } else if (validacao === 1) {
+            alert("A senha deve conter pelo menos 6 caracteres.");
+        } else if (validacao === 2) {
+            alert("É necessário inserir sobrenome.")
         }
     });
 });
+
+
